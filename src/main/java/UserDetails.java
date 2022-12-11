6e667da5526aa2c99183afb99c8f18beefcc4459
nisha <nishantsingh187@gmail.com>
2022-12-12 04:17:25 +0530

    Customised Exception

--- src/main/java/UserDetails.java
+++ src/main/java/UserDetails.java
@@ -1,4 +1,3 @@
-
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -14,6 +13,22 @@ public class UserDetails {
         return matcher.matches();
     }
 
+    public boolean testFirstName(String firstName) {
+        try {
+            if (!firstName(firstName)) {
+                throw new InvalidInputException("Entered FirstName is Invalid\n" +
+                        "First name starts with Capital Letter and has\n" +
+                        "minimum 3 characters");
+            } else {
+                System.out.println("Entered FirstName is Valid");
+            }
+        } catch (InvalidInputException e) {
+            System.out.println("Exception is Occurred " + e);
+            e.printStackTrace();
+        }
+        return firstName(firstName);
+    }
+
 
     public boolean lastName(String lastName) {
         regex = "^[A-Z]{1}[a-z]{2,}$";
@@ -22,6 +37,21 @@ public class UserDetails {
         return matcher.matches();
     }
 
+    public boolean testLastName(String lastName)  {
+        try {
+            if (!lastName(lastName)) {
+                throw new InvalidInputException("Entered LastName is Invalid\n" +
+                        "Last name starts with Capital Letter and has\n" +
+                        "minimum 3 characters");
+            } else {
+                System.out.println("Entered LastName is Valid");
+            }
+        } catch (InvalidInputException e) {
+            System.out.println("Exception is Occurred " + e);
+            e.printStackTrace();
+        }
+        return lastName(lastName);
+    }
 
     public boolean email(String email) {
         regex = "^[a-z0-9]+([.][a-z0-9]+)*@bl[.]co[.][a-z]{2,3}$";
@@ -30,6 +60,7 @@ public class UserDetails {
         return matcher.matches();
 
     }
+
 
     public boolean mobile(String mobile) {
         regex = "^[0-9]{2}(\\s){1}[0-9]{10}$";
@@ -45,10 +76,5 @@ public class UserDetails {
         Matcher matcher = pattern.matcher(password);
         return matcher.matches();
     }
-    public boolean upperCase(String upperCase) {
-        regex = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&]{1})[A-Za-z\\d@$!%*?&]{8,}$";
-        Pattern pattern = Pattern.compile(regex);
-        Matcher matcher = pattern.matcher(upperCase);
-        return matcher.matches();
-    }
+
 }
